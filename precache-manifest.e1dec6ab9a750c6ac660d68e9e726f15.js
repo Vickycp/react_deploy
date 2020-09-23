cf87c339ff92d97fa7c184df901335fdda3879bd
vickycp <vickycp417@gmail.com>
2020-09-23 17:00:10 +0530

    Updates

---
+++ precache-manifest.e1dec6ab9a750c6ac660d68e9e726f15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a930bde5f4f4cdea01308b41b9db0f73",
+    "url": "/react_deploy/index.html"
+  },
+  {
+    "revision": "e5b7443f2e9064e5cff7",
+    "url": "/react_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react_deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b7443f2e9064e5cff7",
+    "url": "/react_deploy/static/js/main.fa273e35.chunk.js"
+  },
+  {
+    "revision": "781bfd42c5ab447386fd",
+    "url": "/react_deploy/static/js/runtime-main.ec7bf7b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
